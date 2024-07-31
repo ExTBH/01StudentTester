f5a4edb1714f37abde406583e7f8e27aab2faef9
ExTBH <natheer@extbh.dev>
2024-07-31 18:34:47 +0300

    Switch to grouping by Groups

--- app/api/tree/route.ts
+++ app/api/tree/route.ts
@@ -4,57 +4,62 @@ import { Checkpoint } from "@/app/types";
 
 let tests: TreeNode[];
 
-
 export async function GET(req: Request) {
-    if (tests) {
-        return NextResponse.json(tests);
+  if (tests) {
+    return NextResponse.json(tests);
+  }
+
+  let checkpoint: Checkpoint;
+
+  try {
+    // revalidate every 5 hours
+    const response = await fetch(
+      "https://learn.reboot01.com/api/object/bahrain",
+      { next: { revalidate: 1 } }
+    );
+    const data = await response.json();
+    checkpoint = data["children"]["bh-module"]["children"][
+      "checkpoint"
+    ] as Checkpoint;
+  } catch (error) {
+    return NextResponse.json(
+      { error: `An error occurred while fetching tree data: ${error}` },
+      { status: 500 }
+    );
+  }
+
+  const nodes: TreeNode[] = [];
+
+  const groupedNodes: { [prog: number]: TreeNode[] } = {};
+
+  for (const [key, question] of Object.entries(checkpoint.children)) {
+    const group = question.attrs.group;
+
+    if (!groupedNodes[group]) {
+      groupedNodes[group] = [];
     }
 
-    let checkpoint: Checkpoint
+    const node: TreeNode = {
+      key,
+      label: question.name,
+      children: [],
+      data: question,
+    };
 
-    try {
-        // revalidate every 5 hours
-        const response = await fetch('https://learn.reboot01.com/api/object/bahrain', {next: { revalidate: 18000 }})
-        const data = await response.json();
-        checkpoint = data['children']['bh-module']['children']['checkpoint'] as Checkpoint;
-    } catch (error) {
-        return NextResponse.json({ error: `An error occurred while fetching tree data: ${error}` }, { status: 500 });
-    }
+    groupedNodes[group].push(node);
+  }
 
-    const nodes: TreeNode[] = [];
+  for (const group in groupedNodes) {
+    const groupNode: TreeNode = {
+      key: `group-${group}`,
+      label: `Level ${group}`,
+      children: groupedNodes[group],
+    };
 
-    const groupedNodes: { [prog: number]: TreeNode[] } = {};
+    nodes.push(groupNode);
+  }
 
-    for (const [key, question] of Object.entries(checkpoint.children)) {
-        const prog = question.attrs.baseSkills.prog;
+  tests = nodes;
 
-        if (!groupedNodes[prog]) {
-            groupedNodes[prog] = [];
-        }
-
-        const node: TreeNode = {
-            key,
-            label: question.name,
-            children: [],
-            data: question
-        };
-
-        groupedNodes[prog].push(node);
-    }
-
-    let multiplier = 1;
-    for (const prog in groupedNodes) {
-        const groupNode: TreeNode = {
-            key: `prog-${prog}`,
-            label: `Level ${Number.parseInt(prog)-(9 * multiplier)}`,
-            children: groupedNodes[prog],
-        };
-        multiplier++;
-
-        nodes.push(groupNode);
-    }
-
-    tests = nodes;
-
-    return NextResponse.json(nodes);
+  return NextResponse.json(nodes);
 }
